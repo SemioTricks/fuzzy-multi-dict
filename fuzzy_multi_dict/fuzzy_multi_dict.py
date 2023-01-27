526643ac092a1e4a4fae7f346c0c41a0443d2945
tv <lytvynenkotv@gmail.com>
2023-01-27 13:38:11 +0200

    split .get() on subfunctions

--- fuzzy_multi_dict/fuzzy_multi_dict.py
+++ fuzzy_multi_dict/fuzzy_multi_dict.py
@@ -41,12 +41,10 @@ class FuzzyMultiDict:
 
     def get(self, key: str) -> List[Dict[Any, Any]]:
 
-        key_length = len(key)
         max_mistakes_number = self.__max_mistakes_number
 
-        # if its correct number title
         node, position = self.__apply_string(node=self.__prefix_tree, s=key, position=0)
-        if position == key_length and node.get("value"):
+        if position == len(key) and node.get("value"):
             return [
                 {"value": node["value"], "key": key, "mistakes": list()},
             ]
@@ -63,124 +61,38 @@ class FuzzyMultiDict:
             for (position, node, mistakes) in rows_to_process:
                 processed[(position, node["path"])] = len(mistakes)
 
-                if position == key_length and node.get("value"):
-                    __result_row = result.get(node["path"])
-                    if __result_row is None or len(
-                        __result_row["mistakes"] > len(mistakes)
-                    ):
-                        result[node["path"]] = {
-                            "value": node["value"],
-                            "key": node["path"],
-                            "mistakes": mistakes,
-                        }
-                        if len(mistakes) < max_mistakes_number:
-                            max_mistakes_number = len(mistakes)
+                res_row__ = self.__check_value(node, key, position, mistakes, result)
+                if res_row__:
+                    result[node["path"]] = res_row__
+                    if len(mistakes) < max_mistakes_number:
+                        max_mistakes_number = len(mistakes)
                     continue
 
-                __node_children = node["children"].keys()
-                __has_children = len(__node_children) > 0
-
-                # no mistake
-                if position + 1 < key_length and __has_children:
-                    __node = node["children"].get(key[position])
-                    if __node:
-                        __processed = processed.get((position + 1, __node["path"]))
-                        if __processed is None or __processed > len(mistakes):
-                            rows_to_process__.append((position + 1, __node, mistakes))
-                            processed[(position + 1, __node["path"])] = len(mistakes)
-
-                            __node, __position = self.__apply_string(
-                                node=node, s=key, position=position
-                            )
-                            __processed = processed.get((__position, __node["path"]))
-                            if __processed is None or __processed > len(mistakes):
-                                rows_to_process__.append((__position, __node, mistakes))
-                                processed[(__position, __node["path"])] = len(mistakes)
+                rows_to_process__.extend(
+                    self.__no_mistakes(node, key, position, processed, mistakes)
+                )
 
                 if len(mistakes) >= max_mistakes_number:
                     continue
 
-                # mistake: missing symbol
-                if __has_children:
-                    for __c in __node_children:
-                        __node = node["children"][__c]
-                        __mistakes = mistakes + [
-                            {
-                                "mistake_type": f'missing symbol "{__c}"',
-                                "position": position,
-                            },
-                        ]
-                        __processed = processed.get((position, __node["path"]))
-                        if __processed is None or __processed > len(__mistakes):
-                            rows_to_process__.append((position, __node, __mistakes))
-                            processed[(position, __node["path"])] = len(__mistakes)
+                rows_to_process__.extend(
+                    self.__missing_symbol(node, key, position, processed, mistakes)
+                )
 
-                        __node, __position = self.__apply_string(
-                            node=__node, s=key, position=position
-                        )
-                        __processed = processed.get((__position, __node["path"]))
-                        if __processed is None or __processed > len(__mistakes):
-                            rows_to_process__.append((__position, __node, __mistakes))
-                            processed[(__position, __node["path"])] = len(__mistakes)
-
-                if position < key_length:
-
-                    # mistake: extra symbol
-                    __mistakes = mistakes + [
-                        {
-                            "mistake_type": f'extra symbol "{key[position]}"',
-                            "position": position,
-                        },
-                    ]
-                    __processed = processed.get((position + 1, node["path"]))
-                    if __processed is None or __processed > len(__mistakes):
-                        rows_to_process__.append((position + 1, node, __mistakes))
-                        processed[(position + 1, node["path"])] = len(__mistakes)
-
-                    __node, __position = self.__apply_string(
-                        node=node, s=key, position=position + 1
+                if position < len(key):
+                    rows_to_process__.extend(
+                        self.__extra_symbol(node, key, position, processed, mistakes)
                     )
-                    __processed = processed.get((__position, __node["path"]))
-                    if __processed is None or __processed > len(__mistakes):
-                        rows_to_process__.append((__position, __node, __mistakes))
-                        processed[(__position, __node["path"])] = len(__mistakes)
-
-                    # mistake: wrong symbol
-                    for __c in __node_children:
-                        __mistakes = mistakes + [
-                            {
-                                "mistake_type": f'wrong symbol "{key[position]}": '
-                                f'replaced on "{__c}"',
-                                "position": position,
-                            },
-                        ]
-                        __node = node["children"][__c]
-                        __processed = processed.get((position + 1, __node["path"]))
-                        if __processed is None or __processed > len(__mistakes):
-                            rows_to_process__.append((position + 1, __node, __mistakes))
-                            processed[(position + 1, __node["path"])] = len(__mistakes)
-
-                        __node, __position = self.__apply_string(
-                            node=__node, s=key, position=position + 1
-                        )
-                        __processed = processed.get((__position, __node["path"]))
-                        if __processed is None or __processed > len(__mistakes):
-                            rows_to_process__.append((__position, __node, __mistakes))
-                            processed[(__position, __node["path"])] = len(__mistakes)
+                    rows_to_process__.extend(
+                        self.__wrong_symbol(node, key, position, processed, mistakes)
+                    )
 
             if not len(rows_to_process__):
                 break
 
             rows_to_process = rows_to_process__
 
-        if not len(result):
-            return list()
-
-        __min_n_mistakes = min([len(x["mistakes"]) for x in result.values()])
-        return sorted(
-            [x for x in result.values() if len(x["mistakes"]) == __min_n_mistakes],
-            key=lambda x: x["key"],
-        )
+        return self.__prepare_result(result)
 
     def __getitem__(self, key: str) -> Dict[str, Any]:
 
@@ -209,3 +121,137 @@ class FuzzyMultiDict:
             sub_position += 1
 
         return node, position + sub_position
+
+    def __no_mistakes(self, node, key, position, processed, mistakes) -> list:
+
+        __node_children = node["children"].keys()
+        __has_children = len(__node_children) > 0
+
+        rows_to_process__ = list()
+
+        if position + 1 < len(key) and __has_children:
+            __node = node["children"].get(key[position])
+            if __node:
+                __processed = processed.get((position + 1, __node["path"]))
+                if __processed is None or __processed > len(mistakes):
+                    rows_to_process__.append((position + 1, __node, mistakes))
+                    processed[(position + 1, __node["path"])] = len(mistakes)
+
+                    __node, __position = self.__apply_string(
+                        node=node, s=key, position=position
+                    )
+                    __processed = processed.get((__position, __node["path"]))
+                    if __processed is None or __processed > len(mistakes):
+                        rows_to_process__.append((__position, __node, mistakes))
+                        processed[(__position, __node["path"])] = len(mistakes)
+
+        return rows_to_process__
+
+    def __missing_symbol(self, node, key, position, processed, mistakes) -> list:
+
+        rows_to_process__: List[Tuple[int, dict, list]] = list()
+
+        __node_children = node["children"].keys()
+        __has_children = len(__node_children) > 0
+
+        if not __has_children:
+            return rows_to_process__
+
+        for __c in __node_children:
+            __node = node["children"][__c]
+            __mistakes = mistakes + [
+                {
+                    "mistake_type": f'missing symbol "{__c}"',
+                    "position": position,
+                },
+            ]
+            __processed = processed.get((position, __node["path"]))
+            if __processed is None or __processed > len(__mistakes):
+                rows_to_process__.append((position, __node, __mistakes))
+                processed[(position, __node["path"])] = len(__mistakes)
+
+            __node, __position = self.__apply_string(
+                node=__node, s=key, position=position
+            )
+            __processed = processed.get((__position, __node["path"]))
+            if __processed is None or __processed > len(__mistakes):
+                rows_to_process__.append((__position, __node, __mistakes))
+                processed[(__position, __node["path"])] = len(__mistakes)
+
+        return rows_to_process__
+
+    def __extra_symbol(self, node, key, position, processed, mistakes) -> list:
+        rows_to_process__ = list()
+        __mistakes = mistakes + [
+            {
+                "mistake_type": f'extra symbol "{key[position]}"',
+                "position": position,
+            },
+        ]
+
+        __processed = processed.get((position + 1, node["path"]))
+        if __processed is None or __processed > len(__mistakes):
+            rows_to_process__.append((position + 1, node, __mistakes))
+            processed[(position + 1, node["path"])] = len(__mistakes)
+
+        __node, __position = self.__apply_string(
+            node=node, s=key, position=position + 1
+        )
+        __processed = processed.get((__position, __node["path"]))
+        if __processed is None or __processed > len(__mistakes):
+            rows_to_process__.append((__position, __node, __mistakes))
+            processed[(__position, __node["path"])] = len(__mistakes)
+
+        return rows_to_process__
+
+    def __wrong_symbol(self, node, key, position, processed, mistakes) -> list:
+        rows_to_process__ = list()
+
+        __node_children = node["children"].keys()
+
+        for __c in __node_children:
+            __mistakes = mistakes + [
+                {
+                    "mistake_type": f'wrong symbol "{key[position]}": '
+                    f'replaced on "{__c}"',
+                    "position": position,
+                },
+            ]
+            __node = node["children"][__c]
+            __processed = processed.get((position + 1, __node["path"]))
+            if __processed is None or __processed > len(__mistakes):
+                rows_to_process__.append((position + 1, __node, __mistakes))
+                processed[(position + 1, __node["path"])] = len(__mistakes)
+
+            __node, __position = self.__apply_string(
+                node=__node, s=key, position=position + 1
+            )
+            __processed = processed.get((__position, __node["path"]))
+            if __processed is None or __processed > len(__mistakes):
+                rows_to_process__.append((__position, __node, __mistakes))
+                processed[(__position, __node["path"])] = len(__mistakes)
+
+        return rows_to_process__
+
+    @staticmethod
+    def __prepare_result(result: Dict[str, Dict[Any, Any]]) -> List[Dict[Any, Any]]:
+
+        if not len(result):
+            return list()
+
+        __min_n_mistakes = min([len(x["mistakes"]) for x in result.values()])
+        return sorted(
+            [x for x in result.values() if len(x["mistakes"]) == __min_n_mistakes],
+            key=lambda x: x["key"],
+        )
+
+    def __check_value(self, node, key, position, mistakes, result) -> Optional[dict]:
+        if position == len(key) and node.get("value"):
+            __result_row = result.get(node["path"])
+            if __result_row is None or len(__result_row["mistakes"] > len(mistakes)):
+                return {
+                    "value": node["value"],
+                    "key": node["path"],
+                    "mistakes": mistakes,
+                }
+        return None
