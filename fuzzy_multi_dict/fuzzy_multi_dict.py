8760a974a92a26e9cfe6a11fcd5015e492e75961
Denis Shchtskyi <denis.shchutskyi@branderstudio.com>
2023-01-27 12:20:45 +0200

    small fixes

--- fuzzy_multi_dict/fuzzy_multi_dict.py
+++ fuzzy_multi_dict/fuzzy_multi_dict.py
@@ -1,95 +1,101 @@
-from typing import List, Any, Dict, Optional, Callable
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 
 class FuzzyMultiDict:
-
-    def __init__(self,
-                 max_mistakes_number: int = 2,
-                 update_value_func: Optional[Callable] = None):
+    def __init__(
+        self, max_mistakes_number: int = 2, update_value_func: Optional[Callable] = None
+    ):
         """
         :param max_mistakes_number: the maximum number of mistakes in the key
         :param update_value_func: value merge function when writing a new value by key
 
         """
         self.__prefix_tree = {
-            'parent': 'ROOT',
-            'children': dict(),
-            'data': None,
-            'path': ''
+            "parent": "ROOT",
+            "children": dict(),
+            "data": None,
+            "path": "",
         }
         self.__max_mistakes_number = max_mistakes_number
         self.__update_value = (
-            lambda x, y: y) if update_value_func is None else update_value_func
+            (lambda x, y: y) if update_value_func is None else update_value_func
+        )
 
     def __setitem__(self, key: str, value: Any):
 
         if not isinstance(key, str):
-            raise TypeError(f'Invalid key type: expect str, got {type(key)}')
+            raise TypeError(f"Invalid key type: expect str, got {type(key)}")
 
         __node = self.__prefix_tree
         for i, c in enumerate(key):
-            if __node['children'].get(c) is None:
-                __node['children'][c] = {
-                    'parent': __node,
-                    'children': dict(),
-                    'data': None,
-                    'path': key[:i + 1]
+            if __node["children"].get(c) is None:  # type: ignore
+                __node["children"][c] = {  # type: ignore
+                    "parent": __node,
+                    "children": dict(),
+                    "data": None,
+                    "path": key[: i + 1],
                 }
-            __node = __node['children'][c]
+            __node = __node["children"][c]  # type: ignore
 
-        __node['value'] = self.__update_value(__node.get('value'), value)
+        __node["value"] = self.__update_value(__node.get("value"), value)  # type: ignore  # noqa
 
-    def get(self, key: str) -> list:
+    def get(self, key: str) -> List[Dict[Any, Any]]:
 
         key_length = len(key)
         max_mistakes_number = self.__max_mistakes_number
 
         # if its correct number title
         node, position = self.__apply_string(node=self.__prefix_tree, s=key, position=0)
-        if position == key_length and node.get('value'):
-            return [{'value': node['value'], 'key': key, 'mistakes': list()}, ]
+        if position == key_length and node.get("value"):
+            return [
+                {"value": node["value"], "key": key, "mistakes": list()},
+            ]
 
-        result = dict()
-        rows_to_process = [(position, node, list()), (0, self.__prefix_tree, list())]
-        processed = {(pos, node['path']): 0 for (pos, node, _) in rows_to_process}
+        result = dict()  # type: Dict[Any, Any]
+        rows_to_process = [
+            (position, node, list()),
+            (0, self.__prefix_tree, list()),
+        ]  # type: List[Tuple[int, Dict[Any,Any], List[Any]]]
+        processed = {(pos, node["path"]): 0 for (pos, node, _) in rows_to_process}
 
         while True:
             rows_to_process__ = list()
             for (position, node, mistakes) in rows_to_process:
-                processed[(position, node['path'])] = len(mistakes)
+                processed[(position, node["path"])] = len(mistakes)
 
-                if position == key_length and node.get('value'):
-                    __result_row = result.get(node['path'])
-                    if __result_row is None \
-                            or len(__result_row['mistakes'] > len(mistakes)):
-                        result[node['path']] = {
-                            'value': node['value'],
-                            'key': node['path'],
-                            'mistakes': mistakes
+                if position == key_length and node.get("value"):
+                    __result_row = result.get(node["path"])
+                    if __result_row is None or len(
+                        __result_row["mistakes"] > len(mistakes)
+                    ):
+                        result[node["path"]] = {
+                            "value": node["value"],
+                            "key": node["path"],
+                            "mistakes": mistakes,
                         }
                         if len(mistakes) < max_mistakes_number:
                             max_mistakes_number = len(mistakes)
                     continue
 
-                __node_children = node['children'].keys()
+                __node_children = node["children"].keys()
                 __has_children = len(__node_children) > 0
 
                 # no mistake
                 if position + 1 < key_length and __has_children:
-                    __node = node['children'].get(key[position])
+                    __node = node["children"].get(key[position])
                     if __node:
-                        __processed = processed.get((position + 1, __node['path']))
+                        __processed = processed.get((position + 1, __node["path"]))
                         if __processed is None or __processed > len(mistakes):
                             rows_to_process__.append((position + 1, __node, mistakes))
-                            processed[(position + 1, __node['path'])] = len(mistakes)
+                            processed[(position + 1, __node["path"])] = len(mistakes)
 
                             __node, __position = self.__apply_string(
                                 node=node, s=key, position=position
                             )
-                            __processed = processed.get((__position, __node['path']))
+                            __processed = processed.get((__position, __node["path"]))
                             if __processed is None or __processed > len(mistakes):
                                 rows_to_process__.append((__position, __node, mistakes))
-                                processed[(__position, __node['path'])] = len(mistakes)
+                                processed[(__position, __node["path"])] = len(mistakes)
 
                 if len(mistakes) >= max_mistakes_number:
                     continue
@@ -97,61 +103,70 @@ class FuzzyMultiDict:
                 # mistake: missing symbol
                 if __has_children:
                     for __c in __node_children:
-                        __node = node['children'][__c]
-                        __mistakes = mistakes + [{
-                            'mistake_type': f'missing symbol "{__c}"',
-                            'position': position},
+                        __node = node["children"][__c]
+                        __mistakes = mistakes + [
+                            {
+                                "mistake_type": f'missing symbol "{__c}"',
+                                "position": position,
+                            },
                         ]
-                        __processed = processed.get((position, __node['path']))
+                        __processed = processed.get((position, __node["path"]))
                         if __processed is None or __processed > len(__mistakes):
                             rows_to_process__.append((position, __node, __mistakes))
-                            processed[(position, __node['path'])] = len(__mistakes)
+                            processed[(position, __node["path"])] = len(__mistakes)
 
-                        __node, __position = self.__apply_string(node=__node, s=key,
-                                                                 position=position)
-                        __processed = processed.get((__position, __node['path']))
+                        __node, __position = self.__apply_string(
+                            node=__node, s=key, position=position
+                        )
+                        __processed = processed.get((__position, __node["path"]))
                         if __processed is None or __processed > len(__mistakes):
                             rows_to_process__.append((__position, __node, __mistakes))
-                            processed[(__position, __node['path'])] = len(__mistakes)
+                            processed[(__position, __node["path"])] = len(__mistakes)
 
                 if position < key_length:
 
                     # mistake: extra symbol
-                    __mistakes = mistakes + [{
-                        'mistake_type': f'extra symbol "{key[position]}"',
-                        'position': position},
+                    __mistakes = mistakes + [
+                        {
+                            "mistake_type": f'extra symbol "{key[position]}"',
+                            "position": position,
+                        },
                     ]
-                    __processed = processed.get((position + 1, node['path']))
+                    __processed = processed.get((position + 1, node["path"]))
                     if __processed is None or __processed > len(__mistakes):
                         rows_to_process__.append((position + 1, node, __mistakes))
-                        processed[(position + 1, node['path'])] = len(__mistakes)
+                        processed[(position + 1, node["path"])] = len(__mistakes)
 
                     __node, __position = self.__apply_string(
-                        node=node, s=key, position=position + 1)
-                    __processed = processed.get((__position, __node['path']))
+                        node=node, s=key, position=position + 1
+                    )
+                    __processed = processed.get((__position, __node["path"]))
                     if __processed is None or __processed > len(__mistakes):
                         rows_to_process__.append((__position, __node, __mistakes))
-                        processed[(__position, __node['path'])] = len(__mistakes)
+                        processed[(__position, __node["path"])] = len(__mistakes)
 
                     # mistake: wrong symbol
                     for __c in __node_children:
-                        __mistakes = mistakes + [{
-                            'mistake_type': f'wrong symbol "{key[position]}": '
-                                            f'replaced on "{__c}"',
-                            'position': position},
+                        __mistakes = mistakes + [
+                            {
+                                "mistake_type": f'wrong symbol "{key[position]}": '
+                                f'replaced on "{__c}"',
+                                "position": position,
+                            },
                         ]
-                        __node = node['children'][__c]
-                        __processed = processed.get((position + 1, __node['path']))
+                        __node = node["children"][__c]
+                        __processed = processed.get((position + 1, __node["path"]))
                         if __processed is None or __processed > len(__mistakes):
                             rows_to_process__.append((position + 1, __node, __mistakes))
-                            processed[(position + 1, __node['path'])] = len(__mistakes)
+                            processed[(position + 1, __node["path"])] = len(__mistakes)
 
                         __node, __position = self.__apply_string(
-                            node=__node, s=key, position=position + 1)
-                        __processed = processed.get((__position, __node['path']))
+                            node=__node, s=key, position=position + 1
+                        )
+                        __processed = processed.get((__position, __node["path"]))
                         if __processed is None or __processed > len(__mistakes):
                             rows_to_process__.append((__position, __node, __mistakes))
-                            processed[(__position, __node['path'])] = len(__mistakes)
+                            processed[(__position, __node["path"])] = len(__mistakes)
 
             if not len(rows_to_process__):
                 break
@@ -159,34 +174,34 @@ class FuzzyMultiDict:
             rows_to_process = rows_to_process__
 
         if not len(result):
-            return result
+            return list()
 
-        __min_n_mistakes = min([len(x['mistakes']) for x in result.values()])
+        __min_n_mistakes = min([len(x["mistakes"]) for x in result.values()])
         return sorted(
-            [x for x in result.values() if len(x['mistakes']) == __min_n_mistakes],
-            key=lambda x: x['key']
+            [x for x in result.values() if len(x["mistakes"]) == __min_n_mistakes],
+            key=lambda x: x["key"],
         )
 
-    def __getitem__(self, key: str) -> List[Dict[str, Any]]:
+    def __getitem__(self, key: str) -> Dict[str, Any]:
 
         if not isinstance(key, str):
-            raise TypeError(f'Invalid key type: expect str; got {type(key)}')
+            raise TypeError(f"Invalid key type: expect str; got {type(key)}")
 
         value = self.get(key)
 
         if len(value):
-            return value[0]['value']
+            return value[0]["value"]
 
         raise KeyError(key)
 
     @staticmethod
-    def __apply_string(node: dict, s: str, position: int):
+    def __apply_string(node: dict, s: str, position: int) -> Tuple[Dict[Any, Any], int]:
 
         sub_position = 0
 
         for c in s[position:]:
 
-            __node = node['children'].get(c)
+            __node = node["children"].get(c)
             if not __node:
                 return node, position + sub_position
 
--- tests/test_fuzzy_multi_dict.py
+++ tests/test_fuzzy_multi_dict.py
@@ -7,60 +7,59 @@ def test_get_key_with_mistakes():
 
     d = FuzzyMultiDict(max_mistakes_number=3)
 
-    d['first'] = 1
-    d['second'] = 2
-    d['third'] = 3
+    d["first"] = 1
+    d["second"] = 2
+    d["third"] = 3
 
-    assert d['first'] == 1
-    assert d['frs'] == 1
-    assert d['rs'] == 1
+    assert d["first"] == 1
+    assert d["frs"] == 1
+    assert d["rs"] == 1
 
-    r = d.get('frst')
+    r = d.get("frst")
     assert len(r) == 1
-    assert r[0]['value'] == 1
-    assert len(r[0]['mistakes']) == 1
+    assert r[0]["value"] == 1
+    assert len(r[0]["mistakes"]) == 1
 
 
 def test_get_key_with_alts():
     d = FuzzyMultiDict(max_mistakes_number=3)
 
-    d['first'] = 1
-    d['second'] = 2
-    d['third'] = 3
+    d["first"] = 1
+    d["second"] = 2
+    d["third"] = 3
 
-    r = d.get('fird')
+    r = d.get("fird")
     assert len(r) == 2
-    values = [_['value'] for _ in r]
+    values = [_["value"] for _ in r]
     assert 1 in values and 3 in values
 
 
 def test_get_no_key():
     d = FuzzyMultiDict(max_mistakes_number=3)
 
-    d['first'] = 1
-    d['second'] = 2
+    d["first"] = 1
+    d["second"] = 2
 
-    r = d.get('third')
+    r = d.get("third")
     assert len(r) == 0
 
     with pytest.raises(KeyError):
-        d['third']
+        _ = d["third"]
 
 
 def test_set_dict():
     d = FuzzyMultiDict(max_mistakes_number=3)
 
-    d['first'] = {'x': 1, 'y': 2, 'z': 3}
-    d['second'] = [1, 2, 3]
-    d['third'] = 3
-
-    assert d.get('first') is not None
-    assert d['first'].get('x') is not None
-    assert d['first']['x'] == 1
+    d["first"] = {"x": 1, "y": 2, "z": 3}
+    d["second"] = [1, 2, 3]
+    d["third"] = 3
+    print(d["first"].get("x"))
+    assert d.get("first") is not None
+    assert d["first"].get("x") is not None
+    assert d["first"]["x"] == 1
 
 
 def test_custom_upd_value():
-
     def update_value(x, y):
 
         if x is None:
@@ -68,7 +67,8 @@ def test_custom_upd_value():
 
         if not isinstance(x, dict) or not isinstance(y, dict):
             raise TypeError(
-                f'Invalid value type; expect dict; got {type(x)} and {type(y)}')
+                f"Invalid value type; expect dict; got {type(x)} and {type(y)}"
+            )
 
         for k, v in y.items():
 
@@ -88,16 +88,16 @@ def test_custom_upd_value():
 
     d = FuzzyMultiDict(max_mistakes_number=3, update_value_func=update_value)
 
-    d['first'] = {'x': 1, 'y': 2}
-    d['first'] = {'x': 1, 'y': 2, 'z': 3}
-    assert d['first']['x'] == 1
-    assert d['first']['y'] == 2
+    d["first"] = {"x": 1, "y": 2}
+    d["first"] = {"x": 1, "y": 2, "z": 3}
+    assert d["first"]["x"] == 1
+    assert d["first"]["y"] == 2
 
-    d['first'] = {'x': 1, 'y': 2, 'z': 4}
-    assert d['first']['z'] == [3, 4]
+    d["first"] = {"x": 1, "y": 2, "z": 4}
+    assert d["first"]["z"] == [3, 4]
 
-    d['first'] = {'z': 5}
-    assert d['first']['z'] == [3, 4, 5]
+    d["first"] = {"z": 5}
+    assert d["first"]["z"] == [3, 4, 5]
 
-    d['first'] = {'z': 5}
-    assert d['first']['z'] == [3, 4, 5]
+    d["first"] = {"z": 5}
+    assert d["first"]["z"] == [3, 4, 5]
