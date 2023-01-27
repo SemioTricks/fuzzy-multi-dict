f29e99f72f90901291ef84efee7b38d6832eb197
tv <lytvynenkotv@gmail.com>
2023-01-27 11:49:46 +0200

    changed on pytest

--- tests/test_fuzzy_multi_dict.py
+++ tests/test_fuzzy_multi_dict.py
@@ -1,101 +1,103 @@
-from unittest import TestCase
+import pytest
 
 from fuzzy_multi_dict import FuzzyMultiDict
 
 
-class TestFuzzyMultiDict(TestCase):
+def test_get_key_with_mistakes():
 
-    def test_get_key_with_mistakes(self):
+    d = FuzzyMultiDict(max_mistakes_number=3)
 
-        d = FuzzyMultiDict(max_mistakes_number=3)
+    d['first'] = 1
+    d['second'] = 2
+    d['third'] = 3
 
-        d['first'] = 1
-        d['second'] = 2
-        d['third'] = 3
+    assert d['first'] == 1
+    assert d['frs'] == 1
+    assert d['rs'] == 1
 
-        self.assertEqual(d['first'], 1)
-        self.assertEqual(d['frs'], 1)
-        self.assertEqual(d['rs'], 1)
+    r = d.get('frst')
+    assert len(r) == 1
+    assert r[0]['value'] == 1
+    assert len(r[0]['mistakes']) == 1
 
-        r = d.get('frst')
-        self.assertEqual(len(r), 1)
-        self.assertEqual(r[0]['value'], 1)
-        self.assertEqual(len(r[0]['mistakes']), 1)
 
-    def test_get_key_with_alts(self):
-        d = FuzzyMultiDict(max_mistakes_number=3)
+def test_get_key_with_alts():
+    d = FuzzyMultiDict(max_mistakes_number=3)
 
-        d['first'] = 1
-        d['second'] = 2
-        d['third'] = 3
+    d['first'] = 1
+    d['second'] = 2
+    d['third'] = 3
 
-        r = d.get('fird')
-        self.assertEqual(len(r), 2)
-        values = [_['value'] for _ in r]
-        self.assertTrue(1 in values)
-        self.assertTrue(3 in values)
+    r = d.get('fird')
+    assert len(r) == 2
+    values = [_['value'] for _ in r]
+    assert 1 in values and 3 in values
 
-    def test_get_no_key(self):
-        d = FuzzyMultiDict(max_mistakes_number=3)
 
-        d['first'] = 1
-        d['second'] = 2
+def test_get_no_key():
+    d = FuzzyMultiDict(max_mistakes_number=3)
 
-        r = d.get('third')
-        self.assertEqual(len(r), 0)
+    d['first'] = 1
+    d['second'] = 2
 
-        with self.assertRaises(KeyError):
-            d['third']
+    r = d.get('third')
+    assert len(r) == 0
 
-    def test_set_dict(self):
-        d = FuzzyMultiDict(max_mistakes_number=3)
+    with pytest.raises(KeyError):
+        d['third']
 
-        d['first'] = {'x': 1, 'y': 2, 'z': 3}
-        d['second'] = [1, 2, 3]
-        d['third'] = 3
 
-        self.assertIsNotNone(d.get('first'))
-        self.assertIsNotNone(d['first'].get('x'))
-        self.assertEqual(d['first']['x'], 1)
+def test_set_dict():
+    d = FuzzyMultiDict(max_mistakes_number=3)
 
-    def test_custom_upd_value(self):
+    d['first'] = {'x': 1, 'y': 2, 'z': 3}
+    d['second'] = [1, 2, 3]
+    d['third'] = 3
 
-        def update_value(x, y):
-            if x is None: return y
+    assert d.get('first') is not None
+    assert d['first'].get('x') is not None
+    assert d['first']['x'] == 1
 
-            if not isinstance(x, dict) or not isinstance(y, dict):
-                raise TypeError(
-                    f'Invalid value type; expect dict; got {type(x)} and {type(y)}')
 
-            for k, v in y.items():
-                if x.get(k) is None:
-                    x[k] = v
-                    continue
+def test_custom_upd_value():
 
-                if isinstance(x[k], list):
-                    if v not in x[k]:
-                        x[k].append(v)
-                    continue
+    def update_value(x, y):
 
-                if x[k] != v:
-                    x[k] = [x[k], v]
+        if x is None:
+            return y
 
-            return x
+        if not isinstance(x, dict) or not isinstance(y, dict):
+            raise TypeError(
+                f'Invalid value type; expect dict; got {type(x)} and {type(y)}')
 
-        d = FuzzyMultiDict(max_mistakes_number=3, update_value_func=update_value)
+        for k, v in y.items():
 
-        d['first'] = {'x': 1, 'y': 2}
-        d['first'] = {'x': 1, 'y': 2, 'z': 3}
-        self.assertEqual(d['first']['x'], 1)
-        self.assertEqual(d['first']['y'], 2)
+            if x.get(k) is None:
+                x[k] = v
+                continue
 
-        d['first'] = {'x': 1, 'y': 2, 'z': 4}
-        self.assertEqual(d['first']['x'], 1)
-        self.assertEqual(d['first']['y'], 2)
-        self.assertEqual(d['first']['z'], [3, 4])
+            if isinstance(x[k], list):
+                if v not in x[k]:
+                    x[k].append(v)
+                continue
 
-        d['first'] = {'z': 5}
-        self.assertEqual(d['first']['z'], [3, 4, 5])
+            if x[k] != v:
+                x[k] = [x[k], v]
 
-        d['first'] = {'z': 5}
-        self.assertEqual(d['first']['z'], [3, 4, 5])
+        return x
+
+    d = FuzzyMultiDict(max_mistakes_number=3, update_value_func=update_value)
+
+    d['first'] = {'x': 1, 'y': 2}
+    d['first'] = {'x': 1, 'y': 2, 'z': 3}
+    assert d['first']['x'] == 1
+    assert d['first']['y'] == 2
+
+    d['first'] = {'x': 1, 'y': 2, 'z': 4}
+    assert d['first']['z'] == [3, 4]
+
+    d['first'] = {'z': 5}
+    assert d['first']['z'] == [3, 4, 5]
+
+    d['first'] = {'z': 5}
+    assert d['first']['z'] == [3, 4, 5]
