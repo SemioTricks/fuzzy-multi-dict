74ae9d634e93cc76b174ed5c3aa7cc201cbf8b0f
tv <lytvynenkotv@gmail.com>
2023-01-29 17:53:24 +0200

    removed param path from tree node

--- fuzzy_multi_dict/fuzzy_multi_dict.py
+++ fuzzy_multi_dict/fuzzy_multi_dict.py
@@ -6,15 +6,16 @@ class FuzzyMultiDict:
         self, max_mistakes_number: int = 2, update_value_func: Optional[Callable] = None
     ):
         """
-        :param max_mistakes_number: the maximum number of mistakes in the key
-        :param update_value_func: value merge function when writing a new value by key
+        :param max_mistakes_number: default value of maximum number of corrections
+               in the query key when searching for a matching dictionary key
+        :param update_value_func: merge function for value
+               when storing a new value with a key
 
         """
         self.__prefix_tree = {
             "parent": "ROOT",
             "children": dict(),
             "data": None,
-            "path": "",
         }
         self.__max_mistakes_number = max_mistakes_number
         self.__update_value = (
@@ -22,7 +23,10 @@ class FuzzyMultiDict:
         )
 
     def __setitem__(self, key: str, value: Any):
+        """
+        Storing the `value` with the `key`
 
+        """
         if not isinstance(key, str):
             raise TypeError(f"Invalid key type: expect str, got {type(key)}")
 
@@ -33,15 +37,32 @@ class FuzzyMultiDict:
                     "parent": __node,
                     "children": dict(),
                     "data": None,
-                    "path": key[: i + 1],
                 }
             __node = __node["children"][c]  # type: ignore
 
         __node["value"] = self.__update_value(__node.get("value"), value)  # type: ignore  # noqa
 
-    def get(self, key: str) -> List[Dict[Any, Any]]:
+    def get(
+        self, key: str, max_mistakes_number: Optional[int] = None
+    ) -> List[Dict[Any, Any]]:
+        """
+        Extracting the value given the `key`
 
-        max_mistakes_number = self.__max_mistakes_number
+        :param key: dictionary key
+        :param int max_mistakes_number: maximum number of corrections in the query key
+               when searching for a matching dictionary key
+        :return List[Dict[Any, Any]]:
+            [
+                {
+                    "value": <dictionary value>,
+                    "key": <dictionary key; may differ from the query key>
+                    "mistakes": <list of correction in the query key>
+                },
+                ...
+            ]
+
+        """
+        max_mistakes_number = max_mistakes_number or self.__max_mistakes_number
 
         node, position = self.__apply_string(node=self.__prefix_tree, s=key, position=0)
         if position == len(key) and node.get("value") is not None:
@@ -51,40 +72,47 @@ class FuzzyMultiDict:
 
         result = dict()  # type: Dict[Any, Any]
         rows_to_process = [
-            (position, node, list()),
-            (0, self.__prefix_tree, list()),
-        ]  # type: List[Tuple[int, Dict[Any,Any], List[Any]]]
-        processed = {(pos, node["path"]): 0 for (pos, node, _) in rows_to_process}
+            (position, key[:position], node, list()),
+            (0, "", self.__prefix_tree, list()),
+        ]  # type: List[Tuple[int, str, Dict[Any,Any], List[Any]]]
+        processed = {(pos, path): 0 for (pos, path, _, __) in rows_to_process}
 
         while True:
             rows_to_process__ = list()
-            for (position, node, mistakes) in rows_to_process:
-                processed[(position, node["path"])] = len(mistakes)
+            for (position, path, node, mistakes) in rows_to_process:
 
-                res_row__ = self.__check_value(node, key, position, mistakes, result)
+                res_row__ = self.__check_value(
+                    node, path, key, position, mistakes, result
+                )
                 if res_row__:
-                    result[node["path"]] = res_row__
+                    result[path] = res_row__
                     if len(mistakes) < max_mistakes_number:
                         max_mistakes_number = len(mistakes)
                     continue
 
                 rows_to_process__.extend(
-                    self.__no_mistakes(node, key, position, processed, mistakes)
+                    self.__no_mistakes(node, path, key, position, processed, mistakes)
                 )
 
                 if len(mistakes) >= max_mistakes_number:
                     continue
 
                 rows_to_process__.extend(
-                    self.__missing_symbol(node, key, position, processed, mistakes)
+                    self.__missing_symbol(
+                        node, path, key, position, processed, mistakes
+                    )
                 )
 
                 if position < len(key):
                     rows_to_process__.extend(
-                        self.__extra_symbol(node, key, position, processed, mistakes)
+                        self.__extra_symbol(
+                            node, path, key, position, processed, mistakes
+                        )
                     )
                     rows_to_process__.extend(
-                        self.__wrong_symbol(node, key, position, processed, mistakes)
+                        self.__wrong_symbol(
+                            node, path, key, position, processed, mistakes
+                        )
                     )
 
             if not len(rows_to_process__):
@@ -95,7 +123,13 @@ class FuzzyMultiDict:
         return self.__prepare_result(result)
 
     def __getitem__(self, key: str) -> Dict[str, Any]:
+        """
+        Extracting the value given the `key`
 
+        :param key: dictionary key
+        :return Any: first found dictionary value
+
+        """
         if not isinstance(key, str):
             raise TypeError(f"Invalid key type: expect str; got {type(key)}")
 
@@ -122,8 +156,15 @@ class FuzzyMultiDict:
 
         return node, position + sub_position
 
-    def __no_mistakes(self, node, key, position, processed, mistakes) -> list:
-
+    def __no_mistakes(
+        self,
+        node: dict,
+        path: str,
+        key: str,
+        position: int,
+        processed: dict,
+        mistakes: list,
+    ) -> list:
         __node_children = node["children"].keys()
         __has_children = len(__node_children) > 0
 
@@ -132,24 +173,26 @@ class FuzzyMultiDict:
         if position + 1 < len(key) and __has_children:
             __node = node["children"].get(key[position])
             if __node:
-                __processed = processed.get((position + 1, __node["path"]))
+                __path = path + key[position]
+                __processed = processed.get((position + 1, __path))
                 if __processed is None or __processed > len(mistakes):
-                    rows_to_process__.append((position + 1, __node, mistakes))
-                    processed[(position + 1, __node["path"])] = len(mistakes)
+                    rows_to_process__.append((position + 1, __path, __node, mistakes))
+                    processed[(position + 1, __path)] = len(mistakes)
 
-                    __node, __position = self.__apply_string(
-                        node=node, s=key, position=position
-                    )
-                    __processed = processed.get((__position, __node["path"]))
-                    if __processed is None or __processed > len(mistakes):
-                        rows_to_process__.append((__position, __node, mistakes))
-                        processed[(__position, __node["path"])] = len(mistakes)
+                __node, __position = self.__apply_string(
+                    node=node, s=key, position=position
+                )
+                __path = path + key[position:__position]
+                __processed = processed.get((__position, __path))
+                if __processed is None or __processed > len(mistakes):
+                    rows_to_process__.append((__position, __path, __node, mistakes))
+                    processed[(__position, __path)] = len(mistakes)
 
         return rows_to_process__
 
-    def __missing_symbol(self, node, key, position, processed, mistakes) -> list:
+    def __missing_symbol(self, node, path, key, position, processed, mistakes) -> list:
 
-        rows_to_process__: List[Tuple[int, dict, list]] = list()
+        rows_to_process__: List[Tuple[int, str, dict, list]] = list()
 
         __node_children = node["children"].keys()
         __has_children = len(__node_children) > 0
@@ -159,28 +202,30 @@ class FuzzyMultiDict:
 
         for __c in __node_children:
             __node = node["children"][__c]
+            __path = path + __c
             __mistakes = mistakes + [
                 {
                     "mistake_type": f'missing symbol "{__c}"',
                     "position": position,
                 },
             ]
-            __processed = processed.get((position, __node["path"]))
+            __processed = processed.get((position, __path))
             if __processed is None or __processed > len(__mistakes):
-                rows_to_process__.append((position, __node, __mistakes))
-                processed[(position, __node["path"])] = len(__mistakes)
+                rows_to_process__.append((position, __path, __node, __mistakes))
+                processed[(position, __path)] = len(__mistakes)
 
             __node, __position = self.__apply_string(
                 node=__node, s=key, position=position
             )
-            __processed = processed.get((__position, __node["path"]))
+            __path = path + __c + key[position:__position]
+            __processed = processed.get((__position, __path))
             if __processed is None or __processed > len(__mistakes):
-                rows_to_process__.append((__position, __node, __mistakes))
-                processed[(__position, __node["path"])] = len(__mistakes)
+                rows_to_process__.append((__position, __path, __node, __mistakes))
+                processed[(__position, __path)] = len(__mistakes)
 
         return rows_to_process__
 
-    def __extra_symbol(self, node, key, position, processed, mistakes) -> list:
+    def __extra_symbol(self, node, path, key, position, processed, mistakes) -> list:
         rows_to_process__ = list()
         __mistakes = mistakes + [
             {
@@ -189,27 +234,30 @@ class FuzzyMultiDict:
             },
         ]
 
-        __processed = processed.get((position + 1, node["path"]))
+        __processed = processed.get((position + 1, path))
         if __processed is None or __processed > len(__mistakes):
-            rows_to_process__.append((position + 1, node, __mistakes))
-            processed[(position + 1, node["path"])] = len(__mistakes)
+            rows_to_process__.append((position + 1, path, node, __mistakes))
+            processed[(position + 1, path)] = len(__mistakes)
 
         __node, __position = self.__apply_string(
             node=node, s=key, position=position + 1
         )
-        __processed = processed.get((__position, __node["path"]))
-        if __processed is None or __processed > len(__mistakes):
-            rows_to_process__.append((__position, __node, __mistakes))
-            processed[(__position, __node["path"])] = len(__mistakes)
+        __path = path + key[position + 1 : position]
 
+        __processed = processed.get((__position, __path))
+        if __processed is None or __processed > len(__mistakes):
+            rows_to_process__.append((__position, __path, __node, __mistakes))
+            processed[(__position, __path)] = len(__mistakes)
         return rows_to_process__
 
-    def __wrong_symbol(self, node, key, position, processed, mistakes) -> list:
+    def __wrong_symbol(self, node, path, key, position, processed, mistakes) -> list:
         rows_to_process__ = list()
 
         __node_children = node["children"].keys()
 
         for __c in __node_children:
+            if __c == key[position]:
+                continue
             __mistakes = mistakes + [
                 {
                     "mistake_type": f'wrong symbol "{key[position]}": '
@@ -218,18 +266,22 @@ class FuzzyMultiDict:
                 },
             ]
             __node = node["children"][__c]
-            __processed = processed.get((position + 1, __node["path"]))
+            __path = path + __c
+
+            __processed = processed.get((position + 1, __path))
             if __processed is None or __processed > len(__mistakes):
-                rows_to_process__.append((position + 1, __node, __mistakes))
-                processed[(position + 1, __node["path"])] = len(__mistakes)
+                rows_to_process__.append((position + 1, __path, __node, __mistakes))
+                processed[(position + 1, __path)] = len(__mistakes)
 
             __node, __position = self.__apply_string(
                 node=__node, s=key, position=position + 1
             )
-            __processed = processed.get((__position, __node["path"]))
+            __path = path + __c + key[position + 1 : __position]
+
+            __processed = processed.get((__position, __path))
             if __processed is None or __processed > len(__mistakes):
-                rows_to_process__.append((__position, __node, __mistakes))
-                processed[(__position, __node["path"])] = len(__mistakes)
+                rows_to_process__.append((__position, __path, __node, __mistakes))
+                processed[(__position, __path)] = len(__mistakes)
 
         return rows_to_process__
 
@@ -242,13 +294,14 @@ class FuzzyMultiDict:
         __min_n_mistakes = min([len(x["mistakes"]) for x in result.values()])
         return [x for x in result.values() if len(x["mistakes"]) == __min_n_mistakes]
 
-    def __check_value(self, node, key, position, mistakes, result) -> Optional[dict]:
-        if position == len(key) and node.get("value") is not None:
-            __result_row = result.get(node["path"])
-            if __result_row is None or len(__result_row["mistakes"] > len(mistakes)):
+    @staticmethod
+    def __check_value(node, path, key, position, mistakes, result) -> Optional[dict]:
+        if position == len(key) and node.get("value"):
+            __result_row = result.get(path)
+            if __result_row is None or len(__result_row["mistakes"]) > len(mistakes):
                 return {
                     "value": node["value"],
-                    "key": node["path"],
+                    "key": path,
                     "mistakes": mistakes,
                 }
         return None
