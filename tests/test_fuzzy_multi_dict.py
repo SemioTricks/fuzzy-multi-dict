17b49d3729a1606df52d27e2addcaf9601fcc664
tv <lytvynenkotv@gmail.com>
2023-02-10 21:10:40 +0200

    mistakes->corrections, key->query

--- tests/test_fuzzy_multi_dict.py
+++ tests/test_fuzzy_multi_dict.py
@@ -3,9 +3,9 @@ import pytest
 from fuzzy_multi_dict import FuzzyMultiDict
 
 
-def test_get_key_with_mistakes():
+def test_get():
 
-    d = FuzzyMultiDict(max_mistakes_number=3)
+    d = FuzzyMultiDict(max_corrections=3)
 
     d["first"] = 1
     d["second"] = 2
@@ -22,12 +22,12 @@ def test_get_key_with_mistakes():
     r = d.get("frst")
     assert len(r) == 1
     assert r[0]["value"] == 1
-    assert len(r[0]["mistakes"]) == 1
+    assert len(r[0]["correction"]) == 1
 
 
-def test_max_mistakes_number():
+def test_max_corrections():
 
-    d = FuzzyMultiDict(max_mistakes_number=1)
+    d = FuzzyMultiDict(max_corrections=1)
 
     d["first"] = 1
     d["second"] = 2
@@ -35,23 +35,23 @@ def test_max_mistakes_number():
 
     assert len(d.get("firstt")) > 0
     assert len(d.get("firstttt")) == 0
-    assert len(d.get("firstttt", max_mistakes_number=3)) > 0
-    assert len(d.get("firstttt", max_mistakes_number_part=0.5)) > 0
+    assert len(d.get("firstttt", max_corrections=3)) > 0
+    assert len(d.get("firstttt", max_corrections_relative=0.5)) > 0
 
-    d = FuzzyMultiDict(max_mistakes_number=None, max_mistakes_number_part=None)
+    d = FuzzyMultiDict(max_corrections=None, max_corrections_relative=None)
 
     d["first"] = 1
     d["second"] = 2
     d["third"] = 3
 
     assert len(d.get("firstt")) == 0
-    assert len(d.get("firstt", max_mistakes_number=1)) > 0
-    assert len(d.get("firstt", max_mistakes_number_part=0.5)) > 0
+    assert len(d.get("firstt", max_corrections=1)) > 0
+    assert len(d.get("firstt", max_corrections_relative=0.5)) > 0
 
 
 def test_transposition():
 
-    d = FuzzyMultiDict(max_mistakes_number=3)
+    d = FuzzyMultiDict(max_corrections=3)
 
     d["first"] = 1
     d["second"] = 2
@@ -60,11 +60,52 @@ def test_transposition():
     r = d.get("ifrst")
     assert len(r) == 1
     assert r[0]["value"] == 1
-    assert len(r[0]["mistakes"]) == 1
+    assert len(r[0]["correction"]) == 1
+
+
+def test_insertion():
+
+    d = FuzzyMultiDict(max_corrections=3)
+
+    d["first"] = 1
+    d["second"] = 2
+    d["third"] = 3
+
+    r = d.get("frst")
+    assert len(r) == 1
+    assert r[0]["value"] == 1
+    assert len(r[0]["correction"]) == 1
+
+
+def test_substitution():
+    d = FuzzyMultiDict(max_corrections=3)
+
+    d["first"] = 1
+    d["second"] = 2
+    d["third"] = 3
+
+    r = d.get("ferst")
+    assert len(r) == 1
+    assert r[0]["value"] == 1
+    assert len(r[0]["correction"]) == 1
+
+
+def test_deletion():
+
+    d = FuzzyMultiDict(max_corrections=3)
+
+    d["first"] = 1
+    d["second"] = 2
+    d["third"] = 3
+
+    r = d.get("firstt")
+    assert len(r) == 1
+    assert r[0]["value"] == 1
+    assert len(r[0]["correction"]) == 1
 
 
 def test_get_key_with_alts():
-    d = FuzzyMultiDict(max_mistakes_number=3)
+    d = FuzzyMultiDict(max_corrections=3)
 
     d["first"] = 1
     d["second"] = 2
@@ -77,7 +118,7 @@ def test_get_key_with_alts():
 
 
 def test_get_no_key():
-    d = FuzzyMultiDict(max_mistakes_number=3)
+    d = FuzzyMultiDict(max_corrections=3)
 
     d["first"] = 1
     d["second"] = 2
@@ -90,7 +131,7 @@ def test_get_no_key():
 
 
 def test_set_dict():
-    d = FuzzyMultiDict(max_mistakes_number=3)
+    d = FuzzyMultiDict(max_corrections=3)
 
     d["first"] = {"x": 1, "y": 2, "z": 3}
     d["second"] = [1, 2, 3]
@@ -128,7 +169,7 @@ def test_custom_upd_value():
 
         return x
 
-    d = FuzzyMultiDict(max_mistakes_number=3, update_value_func=update_value)
+    d = FuzzyMultiDict(max_corrections=3, update_value_func=update_value)
 
     d["first"] = {"x": 1, "y": 2}
     d["first"] = {"x": 1, "y": 2, "z": 3}
@@ -146,7 +187,7 @@ def test_custom_upd_value():
 
 
 def test_get_key_with_sim():
-    d = FuzzyMultiDict(max_mistakes_number=3)
+    d = FuzzyMultiDict(max_corrections=3)
 
     d["on"] = 1
     d["one"] = 2
